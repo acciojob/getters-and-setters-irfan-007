c9488a02611086b15d21464b9c572ea9aa69979c
irfan <85334710+irfan-007@users.noreply.github.com>
2024-08-16 00:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Person {}
+class Person {
+	let name;
+	let age;
+	Person(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	getName(){
+		return this.name;
+	}
+	setAge(age){
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(name+" is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(name+" is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
